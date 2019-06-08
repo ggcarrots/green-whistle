32f1d2545bbc1e7930589149d6275a8e67308d7d
creativeBernadka <bfryczkowska@gmail.com>
2019-06-08 21:25:01 +0200

    changing sidebar after click on marker handled

--- server/green-whistle/static/src/EventHandling.js
+++ server/green-whistle/static/src/EventHandling.js
@@ -9,3 +9,14 @@ map.on("load", async function () {
 map.on("resize", async function () {
     askForGeoJSON();
 });
+
+function updateSidebar(feature) {
+    if (feature.properties){
+        $( "#species" ).html(feature.properties.species);
+        $( "#inventory_number" ).html(feature.properties.inventory_number);
+        $( "#status" ).html(feature.properties.status);
+        $( "#management_unit" ).html(feature.properties.management_unit);
+        $( "#updated" ).html(feature.properties.updated);
+        $( "#cut_out_reason" ).html(feature.properties.cut_out_reason);
+    }
+}
